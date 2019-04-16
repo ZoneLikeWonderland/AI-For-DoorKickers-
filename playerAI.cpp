10ade4fd2d5ddfd44bc22f07beae404e8384c824
ZarkLngeW <zhanglw2@shanghaitech.edu.cn>
2019-04-17 00:06:49 +0800

    update state machine

--- playerAI.cpp
+++ playerAI.cpp
@@ -1,4 +1,5 @@
 #include "playerAI.h"
+#include "playerbasic.cpp"
 #include<queue>
 #include<cstring>
 #include<algorithm>
@@ -14,7 +15,6 @@
 
 using namespace std;
 
-Logic * logic;
 
 enum STATE{
     RushToCrystal,
@@ -22,7 +22,8 @@ enum STATE{
     BackToOurTarget,
     RushToBonus,
     ProtectCrystal,
-    WanderBouns
+    WanderBouns,
+    RandomAction
 } state[5];
 
 
@@ -42,52 +43,135 @@ Point ROP[MAXM];
 extern void findthreat(int num);
 extern Point dealthreat(int num, Point v);
 
+/* Behavior layer */
+// forward to point
+void Forward(int num,Point dest,bool flash=true){
+    if(!DONE){
+        Point step;
+        move_smart(num,dest,step);
+        flash_s(num,step);
+    }
+    else{
+        Point pos=GetUnit(num).position;
+        int roplen=Astar(pos,dest,ROP);
+        if(canflash(num)&&flash){
+            flash_s(num,bestjump(roplen,ROP,pos,human_velocity));
+        }
+        else
+        {
+            findthreat(num);
+            Point u=ROP[0]-pos;
+            Point v=dealthreat(num,u);
+            move_relative(num,v);
+        }
+    }
+}
+
+
+/* Character layer */
+// state decide
+void Decide(){
+    // bonus
+    rep(i,2){
+        if(dist(GetUnit(i).position,logic->map.bonus_places[i&1])>=bonus_radius)
+        state[i]=RushToBonus;
+        else
+        {
+            state[i]=RandomAction;
+        }
+    }
+
+    // crystal
+    if(logic->crystal[logic->faction^1].belong==-1){
+        fx(i,2,4){
+            state[i]=RushToCrystal;
+        }
+    }else{
+        if(dist(logic->crystal[logic->faction^1].position,logic->map.target_places[logic->faction])<dist(logic->crystal[logic->faction^1].position,logic->map.target_places[logic->faction^1])*0.6){
+            fx(i,2,4){
+                if(node_retranslate(logic->faction,logic->crystal[logic->faction^1].belong)==i){
+                    state[i]=BackToOurTarget;
+                }else{
+                    state[i]=RushToEnemyTarget;
+                }
+            }
+        }else{
+            fx(i,2,4){
+                if(node_retranslate(logic->faction,logic->crystal[logic->faction^1].belong)==i){
+                    state[i]=BackToOurTarget;
+                }else{
+                    state[i]=ProtectCrystal;
+                }
+            }
+        }
+    }
+}
+
+// state evaluate
+void Eval(int num){
+    if(num==3&&dist(GetUnit(3).position,GetUnit(2).position)<splash_radius)
+    state[num]=RandomAction;
+    if(num==4&&(dist(GetUnit(4).position,GetUnit(2).position)<splash_radius||dist(GetUnit(4).position,GetUnit(3).position)<splash_radius))
+    state[num]=RandomAction;
+
+    switch (state[num])
+    {
+    case RushToCrystal:
+        Forward(num,logic->crystal[logic->faction^1].position);
+        break;
+    case RushToEnemyTarget:
+        Forward(num,logic->map.target_places[logic->faction^1]);
+        break;
+    case BackToOurTarget:
+        Forward(num,logic->map.target_places[logic->faction]);
+        break;
+    case RushToBonus:
+        Forward(num,logic->map.bonus_places[num&1]);
+        break;
+    case ProtectCrystal:
+        if(dist(GetUnit(num).position,logic->crystal[logic->faction^1].position)>flash_distance){
+            Forward(num,logic->crystal[logic->faction^1].position);
+        }else{
+            Forward(num,logic->map.target_places[logic->faction],false);
+        }
+        break;
+    case RandomAction:
+        {
+            double vv;
+            Point v;
+            do{
+                vv=RandDouble()*2*PI;
+                v=Point(cos(vv),sin(vv))*human_velocity;
+            }while(isWall(GetUnit(num).position+v));
+            Forward(num,GetUnit(num).position+v,false);
+            break;
+        }
+    default:
+        break;
+    }
+}
+
 void playerAI() {
-    auto t=clock();
+    auto t0=clock();
+    auto t=t0;
+    // ofstream f("1.txt",ios::app);
+    
     logic = Logic::Instance();
     
     static bool ONCE=true;
-    static int stamp=0;
 
     if(ONCE){
         map=logic->map;
         thread th(initwaypoint);
         th.detach();
         ONCE=false;
+        srand(time(NULL));
     }
 
-    bool dontflash=false;
-    if(logic->frame==1)srand(time(NULL));
-    Point rush=logic->crystal[logic->faction^1].position;
-    if(~logic->crystal[logic->faction^1].belong){
-        rush=logic->map.target_places[logic->faction];
-        dontflash=true;
-    }
-    Point dest[5]={logic->map.bonus_places[0],logic->map.bonus_places[1],rush,rush,rush};
+    Decide();
+    rep(i,5)
+    Eval(i);
 
-    if(!DONE){
-        for(int i=0;i<min(stamp/5+3,5);i++){
-            Point step;
-            move_smart(i,dest[i],step);
-            flash_s(i,step);
-        }
-    }
-    else{
-        rep(i,min(stamp/5+3,5)){
-            Point pos=GetUnit(i).position;
-            int roplen=Astar(pos,dest[i],ROP);
-            if(canflash(i)&&!dontflash)
-            flash_s(i,bestjump(roplen,ROP,pos,human_velocity));
-            else
-            {
-                auto T=clock();
-                findthreat(i);
-                Point u=ROP[0]-pos;
-                Point v=dealthreat(i,u);
-                move_relative(i,v);
-            }
-        }
-    }
     for(int i=0;i<5;i++)
     {
         Point mypos=GetUnit(i).position;
@@ -102,7 +186,8 @@ void playerAI() {
         logic->shoot(i,Point(targ.x+RandDouble(-D,D),targ.y+RandDouble(-D,D)));
         logic->meteor(i,Point(targ.x,targ.y));
     }
-    stamp++;
-    // ofstream f("1.txt",ios::app);
-    // f<<"sum:"<<clock()-t<<'\n';
+
+    
+    // rep(i,5)f<<state[i]<<' ';f<<'\n';
+    // f<<"sum:"<<clock()-t0<<'\n';
 }
